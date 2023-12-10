4e571718713f4db0ff6429d2508ad47f1f08b67d
MustafaHasanat <mustafa.hasanat99@gmail.com>
2023-12-10 15:53:52 +0300

    adding test cases

--- src/utils/validators/stringValidators.ts
+++ src/utils/validators/stringValidators.ts
@@ -1,6 +1,15 @@
-const strictNameValidator = (name: string): boolean => {
-    // TODO: complete with regex expressions to validate the naming string
-    return name.includes(" ");
+const tableNameValidator = (name: string): boolean | string => {
+  const hasSpaces = /\s/.test(name);
+  const hasNonAlphanumeric = /[^a-zA-Z]/.test(name);
+  const startsWithUppercase = /^[A-Z]/.test(name);
+
+  if (hasSpaces) return "Name shouldn't contain any space";
+  if (hasNonAlphanumeric)
+    return "Name should only contain letters and/or numbers";
+  if (startsWithUppercase)
+    return "Name shouldn't start with an uppercase letter";
+
+  return true;
 };
 
-export { strictNameValidator };
+export { tableNameValidator };
