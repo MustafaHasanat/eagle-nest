ac48dafed9d4f501bd1e50e120dc198fbcd807f4
MustafaHasanat <mustafa.hasanat99@gmail.com>
2023-12-11 21:13:25 +0300

    fix: fix testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,19 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-    rootDir: "./__tests__",
+    rootDir: ".",
     verbose: true,
-    collectCoverage: true,
-    // collectCoverageFrom: ["src/**/*.{ts,js}"],
-    testMatch: ["**/**/?(*.)+(spec|test).+(ts|tsx|js)"],
+    // collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.(ts|js)"],
+    testMatch: ["**/**/?(*.)+(test).+(ts|js)"],
     moduleFileExtensions: ["ts", "js", "tsx", "jsx", "json", "node"],
     moduleNameMapper: {
-        "(.+)\\.js": "$1",
+        // '^@/(.*)$': './src/$1',
+        "(.+)\\.(ts|js)": "$1",
     },
     transform: {
         "^.+\\.(t|j)sx?$": "ts-jest",
-        // "^.+\\.tsx?$": "ts-jest",
-        // "^.+\\.ts?$": "ts-jest",
     },
+    testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
 export default config;
